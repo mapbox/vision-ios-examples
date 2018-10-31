8a872e873b3f73e6277bee416584b7ec19c121ef
Alexander Pristavko <pristavkoal@gmail.com>
2018-10-31 13:28:57 +0300

    Support pending alert in AlertPlayer

--- demo/AlertPlayer.swift
+++ demo/AlertPlayer.swift
@@ -16,9 +16,14 @@ enum AlertSound {
     case collisionAlertCritical
     case laneDepartureWarning
     
+    @available(*, deprecated, message: "Provide the sound")
+    case newSpeedLimit
+    @available(*, deprecated, message: "Provide the sound")
+    case overSpeedLimit
+    
     static let type = "wav"
     
-    private var path: String {
+    fileprivate var path: String {
         switch self {
         case .collisionAlertWarning:
             return "collision_alert_critical"
@@ -26,74 +31,130 @@ enum AlertSound {
             return "collision_alert_warning"
         case .laneDepartureWarning:
             return "lane_departure_warning"
+        case .newSpeedLimit:
+            return "lane_departure_warning"
+        case .overSpeedLimit:
+            return "lane_departure_warning"
         }
     }
     
-    private var url: URL? {
+    fileprivate var url: URL? {
         guard let url = Bundle.main.url(forResource: path, withExtension: AlertSound.type) else {
             assertionFailure("Alert for name \(path).\(AlertSound.type) is not found")
             return nil
         }
         return url
     }
+}
+
+final class AlertPlayer {
     
-    var soundID: SystemSoundID? {
+    private struct Item: Equatable, CustomStringConvertible {
+        let sound: AlertSound
+        let repeated: Bool
+        
+        var description: String {
+            return "Item(sound: \(sound), repeated: \(repeated))"
+        }
+    }
+    
+    private enum State {
+        case idle
+        case playing(item: Item)
+        case stopped(item: Item)
+    }
+    
+    private var state: State = .idle
+    private var pendingItem: Item?
+    private let soundSource = SoundSource()
+    
+    func play(sound: AlertSound, repeated: Bool = false) {
+        let newItem = Item(sound: sound, repeated: repeated)
+        
+        switch state {
+        case .playing(let item) where item == newItem:
+            return
+        case .playing:
+            stopPlayingCurrent()
+            fallthrough
+        case .stopped:
+            pendingItem = newItem
+        case .idle:
+            startPlaying(newItem)
+        }
+    }
+    
+    func stop() {
+        pendingItem = nil
+        stopPlayingCurrent()
+    }
+    
+    private func startPlaying(_ item: Item) {
+        pendingItem = nil
+        state = .playing(item: item)
+        soundSource.play(item.sound, completion: completion)
+    }
+    
+    private func stopPlayingCurrent() {
+        guard case let .playing(item) = state else { return }
+        state = .stopped(item: item)
+        soundSource.dispose(item.sound)
+    }
+    
+    private func completion() {
+        var itemToPlay: Item?
+        var itemToDispose: Item?
+        
+        switch state {
+        case .playing(let item) where item.repeated:
+            itemToPlay = item
+        case .playing(let item):
+            itemToDispose = item
+        case .stopped, .idle: break
+        }
+        
+        if let item = itemToDispose {
+            soundSource.dispose(item.sound)
+        }
+        
+        if let item = pendingItem ?? itemToPlay {
+            startPlaying(item)
+        } else {
+            state = .idle
+        }
+    }
+}
+
+private class SoundSource {
+    private var cache: [AlertSound: SystemSoundID] = [:]
+    
+    func play(_ sound: AlertSound, completion: (() -> Void)?) {
+        let id = obtainID(for: sound)
+        AudioServicesPlaySystemSoundWithCompletion(id, completion)
+    }
+    
+    func dispose(_ sound: AlertSound) {
+        guard let id = cache.removeValue(forKey: sound) else { return }
+        AudioServicesDisposeSystemSoundID(id)
+    }
+    
+    private func obtainID(for sound: AlertSound) -> SystemSoundID {
+        if let id = cache[sound] {
+            return id
+        } else {
+            let id = createID(for: sound)
+            cache[sound] = id
+            return id
+        }
+    }
+    
+    private func createID(for sound: AlertSound) -> SystemSoundID {
         var soundID: SystemSoundID = 0
-        if let url = url {
+        if let url = sound.url {
             AudioServicesCreateSystemSoundID(url as CFURL, &soundID)
         } else {
             soundID = defaultSound
         }
         return soundID
-    }
-}
-
-final class AlertPlayer {
-    private enum State {
-        case idle
-        case playing(repeated: Bool, soundID: SystemSoundID)
-        case stopped(soundID: SystemSoundID)
-    }
-    
-    private var state: State = .idle
-    
-    func play(sound: AlertSound, repeated: Bool = false) {
-        switch state {
-        case .playing: return
-        case .idle, .stopped: break
-        }
-        
-        guard let soundID = sound.soundID else { return }
-        
-        state = .playing(repeated: repeated, soundID: soundID)
-        
-        startPlaying(soundID: soundID)
-    }
-    
-    func stop() {
-        guard case let .playing(_, soundID) = state else { return }
-        state = .stopped(soundID: soundID)
-    }
-    
-    private func startPlaying(soundID: SystemSoundID) {
-        AudioServicesPlaySystemSoundWithCompletion(soundID, completion)
-    }
-    
-    private func completion() {
-        let soundIDToDelete: SystemSoundID
-        
-        switch state {
-        case let .playing(repeated, soundID) where repeated:
-            startPlaying(soundID: soundID)
-            return
-        case let .playing(repeated, soundID) where !repeated:
-            fallthrough
-        case let .stopped(soundID):
-            soundIDToDelete = soundID
-        default: return
-        }
-        
-        AudioServicesDisposeSystemSoundID(soundIDToDelete)
-        state = .idle
     }
 }
