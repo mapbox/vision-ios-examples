fced16926b0d6ab1cde320c5f777049f9ddf0937
Maksim Vaniukevich <prosticoder@gmail.com>
2019-03-15 18:01:48 +0300

    Safety

--- demo/SafetyState.swift
+++ demo/SafetyState.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import MapboxVisionCore
+import MapboxVisionSafety
 
 enum SafetyState: Equatable {
     
@@ -29,56 +29,57 @@ enum SafetyState: Equatable {
         }
     }
     
-//    struct Collision: Equatable {
-//
-//        enum State {
-//            case warning
-//            case critical
-//
-//            init?(_ state: CollisionState) {
-//                switch state {
-//                case .notTriggered:
-//                    return nil
-//                case .warning:
-//                    self = .warning
-//                case .critical:
-//                    self = .critical
-//                }
-//            }
-//        }
-//
-//        let objectType: ObjectType
-//        let state: State
-//        let boundingBox: CGRect
-//    }
-//
-//    case none
-//    case distance(frame: CGRect, distance: Double, canvasSize: CGSize)
-//    case collisions([Collision], canvasSize: CGSize)
-//
-//    init(_ worldDescription: WorldDescription, canvasSize: CGSize) {
-//
-//        let collisions = worldDescription.collisionObjects.compactMap { collision -> Collision? in
-//
-//            let type = collision.object.detection.objectType
-//            let state = collision.state
-//
-//            guard
-//                let objectType = ObjectType(type),
-//                let collisionState = Collision.State(state)
-//            else { return nil }
-//
-//            let boundingBox = collision.object.detection.boundingBox
-//            return Collision(objectType: objectType, state: collisionState, boundingBox: boundingBox)
-//        }
-//
-//        if collisions.count > 0 {
-//            self = .collisions(collisions, canvasSize: canvasSize)
-//        } else if let car = worldDescription.forwardCar {
-//            let distance = max(0, car.distance - SafetyState.bonnetAdjustment)
-//            self = .distance(frame: car.detection.boundingBox, distance: distance, canvasSize: canvasSize)
-//        } else {
-//            self = .none
-//        }
-//    }
+    struct Collision: Equatable {
+
+        enum State {
+            case warning
+            case critical
+
+            init?(_ level: CollisionDangerLevel) {
+                switch level {
+                case .none:
+                    return nil
+                case .warning:
+                    self = .warning
+                case .critical:
+                    self = .critical
+                }
+            }
+        }
+
+        let objectType: ObjectType
+        let state: State
+        let boundingBox: CGRect
+        let imageSize: ImageSize
+    }
+
+    case none
+    case collisions([Collision])
+
+    init(_ collisionObjects: [CollisionObject]) {
+
+        let collisions = collisionObjects.compactMap { collision -> Collision? in
+
+            let type = collision.object.detectionClass
+            let level = collision.dangerLevel
+
+            guard
+                let objectType = ObjectType(type),
+                let collisionState = Collision.State(level)
+            else { return nil }
+
+            let boundingBox = collision.lastDetection.boundingBox
+            let imageSize = collision.lastFrame.image.size
+            return Collision(objectType: objectType,
+                             state: collisionState,
+                             boundingBox: boundingBox,
+                             imageSize: imageSize)
+        }
+
+        if collisions.count > 0 {
+            self = .collisions(collisions)
+        } else {
+            self = .none
+        }
+    }
 }
